908f9e5805d0998c4b779e525130d83307b93ed6
Esau Jesse <jesseesau13@gmail.com>
2025-07-26 16:20:51 +0100

    Update EditRecipeForm

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -7,7 +7,7 @@ const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    event.preventDefault();
     updateRecipe({ id: recipe.id, title, description });
   };
 
