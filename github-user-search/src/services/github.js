844df18649240b0d28b132e627c0cf2341a06651
Esau Jesse <jesseesau13@gmail.com>
2025-08-01 11:16:15 +0100

    Setup React

---
+++ github-user-search/src/services/github.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/users/';
+const token = import.meta.env.VITE_GITHUB_API_KEY;
+
+export const fetchGitHubUser = async (username) => {
+    try {
+        const headers = token ? { Authorization: `Bearer ${token}` } : {};
+
+        const response = await axios.get(`${BASE_URL}${username}`, { headers });
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            throw new Error('User not found');
+        }
+        throw new Error('Failed to fetch user data');
+    }
+};
