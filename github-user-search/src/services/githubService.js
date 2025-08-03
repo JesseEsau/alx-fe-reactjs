74ce5bb87688c2f182258690ecdcc68c1fcb7e4f
Esau Jesse <jesseesau13@gmail.com>
2025-08-03 16:08:32 +0100

    update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://api.github.com/search/users?q';
 const token = import.meta.env.VITE_GITHUB_API_KEY;
 
-export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
+export const fetchuserData = async ({ username, location, minRepos }) => {
     let query = '';
 
     if (username) query += `${username} in:login`;
