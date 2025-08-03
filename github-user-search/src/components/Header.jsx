27c1434be5f34510238da62a1aeaf85dccbab06c
Esau Jesse <jesseesau13@gmail.com>
2025-08-03 16:03:30 +0100

    Install tailwindcss

--- github-user-search/src/components/Header.jsx
+++ github-user-search/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-      <header className="bg-gray-800 text-white py-4 px-6 shadow-md">
+      <header className="bg-blue-800 text-white py-4 px-6 shadow-md ">
         <h1 className="text-2xl font-bold">GitHub User Search</h1>
       </header>
     );
