4a52dee7f28a5f532e4f17319571606922d09a20
Esau Jesse <jesseesau13@gmail.com>
2025-08-12 09:26:07 +0100

    Setup React with Tailwindcss

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-500 '>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora labore mollitia cumque doloribus, perspiciatis harum ab quod eius, maiores soluta eaque reprehenderit ullam illum quisquam, sit fugit beatae. Voluptatibus, rem.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
